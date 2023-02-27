33824e243e2c4576e6832e00c8889946083606cc
losolkos <zelota23@wp.pl>
2023-02-27 15:22:53 +0100

    Dodaj pliki projektów.

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControler : MonoBehaviour
+{
+    Vector2 movementVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementVector = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, movementVector.x, 0));
+        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime); 
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        movementVector = inputValue.Get<Vector2>();
+
+        Debug.Log(movementVector.ToString());
+    }
+}
