9a35b4975fe8c7eca4ac245ad516e82a84a16a3b
losolkos <zelota23@wp.pl>
2023-03-06 15:24:13 +0100

    ZOmbie jest działo jest JHON SINA JEST

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -6,18 +6,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    public GameObject BulletPrefab;
+    public float bulletSpeed = 20.0f;
+    public float playerSpeed = 2;
     Vector2 movementVector;
+    Transform bulletSpawn;
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
+        bulletSpawn = transform.Find("BulletSpawn");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, movementVector.x, 0));
-        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime); 
+        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime* playerSpeed); 
     }
 
     void OnMove(InputValue inputValue)
@@ -25,5 +30,13 @@ public class PlayerControler : MonoBehaviour
         movementVector = inputValue.Get<Vector2>();
 
         Debug.Log(movementVector.ToString());
+    }
+
+    void OnFire()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, bulletSpawn);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed, ForceMode.VelocityChange);
+        Destroy(bullet, 5);
     }
 }
